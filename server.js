0fe7c52fd61719db1b4736dc06c7b8ed56d7713c
Laszlo Szalai <l.szalai@super-staffing.com>
2023-06-17 19:06:54 +0800

    Feat: add and update cards

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const Redis = require('ioredis');
 const app = express();
 const redis = new Redis('redis://red-ci6l9mp8g3nfucbohhu0:6379');
 
+app.use(express.json()); // Enable JSON request body parsing
+
+// Retrieve all cards
 app.get('/cards', async (req, res) => {
   try {
     // Retrieve the list of card IDs from Redis
@@ -21,6 +24,34 @@ app.get('/cards', async (req, res) => {
   } catch (error) {
     console.error('Error retrieving cards:', error);
     res.status(500).json({ error: 'Failed to retrieve cards' });
+  }
+});
+
+// Create a new card
+app.post('/cards', async (req, res) => {
+  try {
+    const { id, name, description } = req.body;
+    const card = { id, name, description };
+    await redis.hset(`card:${id}`, card);
+    await redis.rpush('cards', id);
+    res.status(201).json(card);
+  } catch (error) {
+    console.error('Error creating card:', error);
+    res.status(500).json({ error: 'Failed to create card' });
+  }
+});
+
+// Update an existing card
+app.put('/cards/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, description } = req.body;
+    const card = { id, name, description };
+    await redis.hset(`card:${id}`, card);
+    res.json(card);
+  } catch (error) {
+    console.error('Error updating card:', error);
+    res.status(500).json({ error: 'Failed to update card' });
   }
 });
 
